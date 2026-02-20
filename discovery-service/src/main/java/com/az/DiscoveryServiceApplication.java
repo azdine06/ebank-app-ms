4fd5386c2b37a98e1f380d12bf5bac78bfd9c10b
azdine06 <oazdine06@gmail.com>
2026-02-20 15:22:27 +0000

    add aplication.properties

--- discovery-service/src/main/java/com/az/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/az/DiscoveryServiceApplication.java
@@ -1,5 +1,10 @@
 package com.az;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
