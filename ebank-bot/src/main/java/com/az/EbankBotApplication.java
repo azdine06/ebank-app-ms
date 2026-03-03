a83318374e720a3a8dcf2849fa0833a77d25bd37
azdine06 <oazdine06@gmail.com>
2026-03-03 23:43:10 +0000

    chore(ebank-bot) : finalisation de la configuration et forçage de la pile Servlet
    - Exclusion de spring-webflux pour imposer le type d'application Servlet
    - Ajout du plugin Maven Spring Boot pour le packaging JAR
    - Configuration de spring.main.web-application-type
    - Mise à jour de la classe principale et activation de la découverte de services
    - Réorganisation des dépendances dans le pom.xml

--- ebank-bot/src/main/java/com/az/EbankBotApplication.java
+++ ebank-bot/src/main/java/com/az/EbankBotApplication.java
@@ -1,7 +1,13 @@
 package com.az;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class EbankBotApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(EbankBotApplication.class, args);
     }
 }
